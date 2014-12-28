7101e0cd9d1d4b3c8c47dadb6423557cbf55fdb3
lukemelo <lukemelo@users.noreply.github.com>
2014-12-27 23:45:40 -0500

    Create batchsub.py
    automated file submission for all files in current directory

---
+++ batchsub.py
@@ -0,0 +1,26 @@
+import os
+
+print 'Creating filedir.txt'
+os.system('ls *.gjf > filedir')
+
+fo = open('filedir')
+input_filenames = fo.readlines()
+fo.close
+
+l_write = []
+for i in range(len(input_filenames)):
+    temp = input_filenames[i].split('.')[0]
+    l_write.append('sqsub -q threaded --mpp=12g -n 8 -r 2d -o ' + temp + '.log g09 ' + temp + '.gjf\n')
+
+fw = open('nlgn_batchsub','w')
+fw.writelines(l_write)
+fw.close()
+
+if len(l_write) > 0:
+    print 'The following is a sample job submission:'
+    print l_write[0]
+    if raw_input('Is this correct? [y,n]: ') == 'y':
+        if raw_input('Submit ' + str(len(l_write)) + ' jobs? [y,n]: ') == 'y':
+            os.system('chmod 757 nlgn_batchsub')
+            os.system('./nlgn_batchsub')
+
